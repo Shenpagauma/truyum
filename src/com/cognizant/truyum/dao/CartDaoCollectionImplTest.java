106cf1c7f11224a0a34c3d020c306fd7c181426e
Uma Sekar <835177@cognizant.com>
2020-01-04 18:13:49 +0530

    java

--- src/com/cognizant/truyum/dao/CartDaoCollectionImplTest.java
+++ src/com/cognizant/truyum/dao/CartDaoCollectionImplTest.java
@@ -37,7 +37,7 @@ public class CartDaoCollectionImplTest {
     }
 
     public static void testRemoveCartItem() {
-        System.out.println("Remove Cart\n");
+        System.out.println("Remove Cart");
         cartDao.removeCartItem(1, 3);
 
     }
